4b01a418ee7972371ebf1a2b4c7fb8b23e092df1
Kevin Lauer <kevin.lauer2019@gmail.com>
2024-09-13 11:07:05 -0500

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,29 @@
+import { defineConfig } from "astro/config";
+import { i18n, filterSitemapByDefaultLocale } from "astro-i18n-aut/integration";
+import sitemap from "@astrojs/sitemap";
+
+const defaultLocale = "en";
+const locales = {
+  en: "en-US", // the `defaultLocale` value must present in `locales` keys
+  es: "es-MX",
+};
+
+export default defineConfig({
+  trailingSlash: "always",
+  build: {
+    format: "directory",
+  },
+  integrations: [
+    i18n({
+      locales,
+      defaultLocale,
+    }),
+    sitemap({
+      i18n: {
+        locales,
+        defaultLocale,
+      },
+      filter: filterSitemapByDefaultLocale({ defaultLocale }),
+    }),
+  ],
+});
